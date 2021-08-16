7f9bf453d03667397fe1363821b8b707e1469103
Taha Dogan Gunes <tdgunes@gmail.com>
2021-08-16 14:34:10 +0300

    Add JustificationAnalysis and JustificationAnalysisExample

---
+++ org-tweetyproject-arg-peaf/src/main/java/org/tweetyproject/arg/peaf/analysis/JustificationAnalysis.java
@@ -0,0 +1,72 @@
+package org.tweetyproject.arg.peaf.analysis;
+
+
+
+import org.tweetyproject.arg.dung.reasoner.AbstractExtensionReasoner;
+import org.tweetyproject.arg.dung.semantics.Extension;
+import org.tweetyproject.arg.dung.syntax.Argument;
+import org.tweetyproject.arg.dung.syntax.DungTheory;
+import org.tweetyproject.arg.peaf.syntax.EAFTheory;
+import org.tweetyproject.arg.peaf.syntax.EArgument;
+import org.tweetyproject.arg.peaf.syntax.InducibleEAF;
+import org.tweetyproject.arg.peaf.syntax.PEAFTheory;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+
+public class JustificationAnalysis {
+
+    public static double computeJustificationOf(Set<EArgument> args, PEAFTheory peafTheory, AbstractExtensionReasoner abstractExtensionReasoner) {
+        AtomicReference<Double> prob = new AtomicReference<>((double) 0);
+
+        Set<String> stringArgs = new HashSet<>();
+
+        for (EArgument arg : args) {
+            stringArgs.add(arg.getName());
+        }
+
+        AtomicInteger i = new AtomicInteger();
+        peafTheory.induceAll((Consumer<InducibleEAF>) ind -> {
+            // If args is in EAFTheory and if X is in an extension of EAFTheory
+            // then induce probability can be considered.
+            EAFTheory eafTheory = ind.toNewEAFTheory();
+            if (eafTheory.getArguments().containsAll(args)) {
+                // If the X is in an extension of IEAF
+
+                DungTheory dungTheory = eafTheory.convertToDAFNaively();
+                Collection<Extension> extensionCollection = abstractExtensionReasoner.getModels(dungTheory);
+
+                for (Extension extension : extensionCollection) {
+                    boolean containsAll = true;
+                    for (Argument argument : extension) {
+                        String mainArgName = argument.getName();
+
+                        if (mainArgName.contains("_")) {
+                            // Must contain all subArgsNames from an extension
+                            String[] subArgsNames = mainArgName.split("_");
+                            for (String subArgsName : subArgsNames) {
+                                containsAll = containsAll && stringArgs.contains(subArgsName);
+                            }
+                        }
+                        else {
+                            containsAll = containsAll && stringArgs.contains(mainArgName);
+                        }
+                    }
+
+                    if (containsAll) {
+                        prob.updateAndGet(v -> (v + ind.getInducePro()));
+                    }
+                }
+
+            }
+
+
+        });
+
+        return prob.get();
+    }
+}
---
+++ org-tweetyproject-arg-peaf/src/main/java/org/tweetyproject/arg/peaf/examples/JustificationAnalysisExample.java
@@ -0,0 +1,62 @@
+package org.tweetyproject.arg.peaf.examples;
+
+import org.tweetyproject.arg.dung.reasoner.SimplePreferredReasoner;
+import org.tweetyproject.arg.peaf.analysis.JustificationAnalysis;
+import org.tweetyproject.arg.peaf.syntax.EArgument;
+import org.tweetyproject.arg.peaf.syntax.InducibleEAF;
+import org.tweetyproject.arg.peaf.syntax.PEAFTheory;
+import org.tweetyproject.arg.peaf.writer.EdgeListWriter;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class JustificationAnalysisExample
+{
+    public static void main(String[] s) {
+        int numOfArgs = 7;
+        PEAFTheory peafTheory = new PEAFTheory();
+
+        for (int i = 0; i < numOfArgs; i++) {
+            peafTheory.addArgument(i);
+        }
+
+
+        peafTheory.addSupport(new int[]{}, new int[]{0}, 1.0);
+        peafTheory.addSupport(new int[]{0}, new int[]{2}, 0.6);
+        peafTheory.addSupport(new int[]{0}, new int[]{1}, 0.7);
+        peafTheory.addSupport(new int[]{0}, new int[]{3}, 0.9);
+        peafTheory.addSupport(new int[]{0}, new int[]{4}, 0.3);
+        peafTheory.addSupport(new int[]{3}, new int[]{5}, 0.5);
+        peafTheory.addSupport(new int[]{3,4}, new int[]{6}, 0.9);
+        peafTheory.addAttack(new int[]{5}, new int[]{2}, 1.0);
+        peafTheory.addAttack(new int[]{5}, new int[]{1}, 1.0);
+        peafTheory.addAttack(new int[]{1}, new int[]{5}, 1.0);
+        peafTheory.addAttack(new int[]{1}, new int[]{6}, 1.0);
+
+
+
+        List<EArgument> args = peafTheory.getArguments();
+        args.get(0).setName("eta");
+        args.get(1).setName("b");
+        args.get(2).setName("d");
+        args.get(3).setName("e");
+        args.get(4).setName("f");
+        args.get(5).setName("a");
+
+
+
+        args.get(6).setName("c");
+
+        Set<EArgument> query = new HashSet<>();
+        query.add(args.get(0));
+        query.add(args.get(2));
+        query.add(args.get(6));
+
+        double probabilisticJustification = JustificationAnalysis.computeJustificationOf(query, peafTheory, new SimplePreferredReasoner());
+        System.out.println("Probabilistic justification: " + probabilisticJustification);
+    }
+
+}
